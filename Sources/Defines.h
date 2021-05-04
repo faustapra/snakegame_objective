c5786f8c7b3013bdc51e3ca23f6dac7841a21523
faustapra <faustapra@gmail.com>
2021-05-04 14:09:02 +0300

    v1
    proceduriskaihjbv

---
+++ Sources/Defines.h
@@ -0,0 +1,11 @@
+#pragma once
+
+
+const int SKAICIUS = 25;
+const int SIZE = 20;
+const int WIDTH = 500;
+const int HEIGHT = 500;
+int game = 0;
+int NUMBER = 3;
+int DIRECTION = 1;
+int score;
---
+++ Sources/Objects.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+#include "defines.h"
+
+
+struct SNAKE {
+	float x;
+	float y;
+} s[100];
+
+
+
+struct FRUIT {
+	float x;
+	float y;
+} f;
